5e68377311ee4563a05e3f1a9390ba0a2acf195d
AM-289 <abbi.murcia@gmail.com>
2025-06-20 15:56:06 +0200

    Creation of the driver profile

---
+++ driver_profile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="driver_profile.css">
+</head>
+<body>
+    <main class="container">
+        <div class="profile">
+            <div class="profile_header">
+                <div class="profile_picture">
+                    <img src="default_profile.png" alt="Profile Picture">
+                </div>
+                <div class="username_display">
+                    <h2>Username: johndoe</h2>
+                </div>
+                <div class="rating_leaves">leaf leaf leaf leaf leaf</div>
+            </div>
+            <div class="line"></div>
+            <div class="profile_content">
+            </div>
+            <div class="line"></div>
+            <div class="profile_details">
+            </div>
+        </div>
+    </main>
+
+    <div class="comments">
+        <h3>Commentaires : </h3>
+    </div>
+</body>
+</html>
