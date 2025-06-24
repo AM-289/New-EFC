e6c3a53807680f87ea5b79b90dfe6c07752836df
AM-289 <abbi.murcia@gmail.com>
2025-06-24 14:30:50 +0200

    Style file added for index.php and search form seperated in its own file

--- PHP/Index.php
+++ PHP/Index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ecoride</title>
+    <link rel="stylesheet" href="../index.css">
 </head>
 <body>
 
@@ -11,6 +12,17 @@
     // Include the header file
     include_once '../HTML/header.html';
 ?>
+<main>
+    <img src="../young-couple-car-trip.jpg" alt="">
+    <?php
+        // Include the search form
+        include_once '../search_form.html';
+    ?>
+</main>
+
+<article>
+
+</article>
 
 <?php
     include_once '../HTML/footer.html';
