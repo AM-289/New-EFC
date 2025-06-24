c281c8e48acd27ae1603ea1c8a9e8419c2cb62a6
AM-289 <abbi.murcia@gmail.com>
2025-06-24 15:41:21 +0200

    Serach results file crated and some style added to the search form

---
+++ PHP/Search_Results.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecoride</title>
+    <link rel="stylesheet" href="../search_results.css">
+</head>
+<body>
+
+<?php
+    // Include the header file
+    include_once '../HTML/header.html';
+?>
+<main>
+    <?php
+        // Include the search form
+        include_once '../search_form.html';
+    ?>
+</main>
+
+<article>
+
+</article>
+
+<?php
+    include_once '../HTML/footer.html';
+?>
+
+<script src="#"></script>
+</body>
+</html>
